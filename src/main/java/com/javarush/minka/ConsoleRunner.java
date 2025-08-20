ae747a1d9dc6c9bc5870a7278597b9b67aae567e
Александр <mnk10@yandex.ru>
2025-08-20 16:13:33 +0300

    Update ConsoleRunner.java

--- src/main/java/com/javarush/minka/ConsoleRunner.java
+++ src/main/java/com/javarush/minka/ConsoleRunner.java
@@ -1,24 +1,12 @@
 package com.javarush.minka;
 
-import com.javarush.khmelov.controller.MainController;
-import com.javarush.khmelov.view.console.ConsoleApp;
-import com.javarush.khmelov.view.console.Menu;
-
 import java.util.Scanner;
 
 public class ConsoleRunner {
 
     public static void main(String[] args) {
-        // 04:12
-        // Осваиваю Git
-
-        //build console app
         Scanner input = new Scanner(System.in);
-        Menu menu = new Menu(input);
-        MainController mainController = new MainController();
-        ConsoleApp application = new ConsoleApp(mainController, menu);
-        //run console app
-        application.run(args);
+
+        input.nextLine(); // Это чтобы предупреждение не высвечивалось.
     }
 }
-
