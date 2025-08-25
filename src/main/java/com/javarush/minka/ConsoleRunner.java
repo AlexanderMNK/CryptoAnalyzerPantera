8d40e13a5126921c548d4c00b11f66e8509c23f5
AlexanderMNK <mnk10@yandex.ru>
2025-08-25 21:14:25 +0300

    Step one. Simple coding and decoding from main.

--- src/main/java/com/javarush/minka/ConsoleRunner.java
+++ src/main/java/com/javarush/minka/ConsoleRunner.java
@@ -1,12 +1,9 @@
 package com.javarush.minka;
 
-import java.util.Scanner;
-
 public class ConsoleRunner {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // ...
-        input.nextLine(); // Это чтобы предупреждение не высвечивалось.
+        // Шаг 1: кодируем текст.
+
     }
 }
