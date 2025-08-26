99d6f77327fb5552eb9c3347750e9d6c7cdd8de7
AlexanderMNK <mnk10@yandex.ru>
2025-08-26 11:04:47 +0300

    Added basic classes.

--- src/main/java/com/javarush/minka/ConsoleRunner.java
+++ src/main/java/com/javarush/minka/ConsoleRunner.java
@@ -3,12 +3,10 @@ package com.javarush.minka;
 public class ConsoleRunner {
 
     public static void main(String[] args) {
-        // Шаг 1: кодируем текст.
-        
-        // Эта строка создана в GitHub.
         String ALPHABET = "тут будет наш алфавит";
         Cipher cipher = new Cipher(ALPHABET.toCharArray());
         cipher.printToConsole();
-
+        new FileManager();
+        new Validator();
     }
 }
